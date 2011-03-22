2da74f5ff9dcbefb14ad8f783dfab849b1d09b4b
Joe Van Dyk <joe@fixieconsulting.com>
2011-03-22 15:17:03 -0700

    Added more cookbooks

---
+++ apache2/recipes/default.rb
@@ -0,0 +1,109 @@
+#
+# Cookbook Name:: apache2
+# Recipe:: default
+#
+# Copyright 2008-2009, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package "apache2" do
+  package_name "apache2"
+  action :install
+end
+
+service "apache2" do
+  service_name "apache2"
+  restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
+  reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
+  supports [:restart, :reload, :status]
+  action :enable
+  subscribes :restart, resources(:execute => "update_rubygems"), :immediately
+end
+
+directory "#{node[:apache][:dir]}/ssl" do
+  action :create
+  mode 0755
+  owner "root"
+  group "root"
+end
+
+template "apache2.conf" do
+  path "#{node[:apache][:dir]}/apache2.conf"
+  source "apache2.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "security" do
+  path "#{node[:apache][:dir]}/conf.d/security"
+  source "security.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  backup false
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "charset" do
+  path "#{node[:apache][:dir]}/conf.d/charset"
+  source "charset.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  backup false
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "#{node[:apache][:dir]}/ports.conf" do
+  source "ports.conf.erb"
+  group "root"
+  owner "root"
+  variables :apache_listen_ports => node[:apache][:listen_ports]
+  mode 0644
+  notifies :restart, resources(:service => "apache2")
+end
+
+template "#{node[:apache][:dir]}/sites-available/default" do
+  source "default-site.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "apache2")
+end
+
+include_recipe "apache2::mod_status"
+include_recipe "apache2::mod_alias"
+include_recipe "apache2::mod_auth_basic"
+include_recipe "apache2::mod_authn_file"
+include_recipe "apache2::mod_authz_default"
+include_recipe "apache2::mod_authz_groupfile"
+include_recipe "apache2::mod_authz_host"
+include_recipe "apache2::mod_authz_user"
+include_recipe "apache2::mod_expires"
+include_recipe "apache2::mod_autoindex"
+include_recipe "apache2::mod_dir"
+include_recipe "apache2::mod_env"
+include_recipe "apache2::mod_mime"
+include_recipe "apache2::mod_negotiation"
+include_recipe "apache2::mod_setenvif"
+include_recipe "apache2::mod_log_config" if platform?("centos", "redhat", "suse")
+
+# uncomment to get working example site on centos/redhat/fedora
+#apache_site "default"
+
+service "apache2" do
+  action :start
+end
---
+++ node_coffee/recipes/default.rb
@@ -0,0 +1,22 @@
+
+# NodeJS
+node_file = "#{Chef::Config[:file_cache_path]}/node.deb"
+remote_file node_file do
+  source   "https://s3.amazonaws.com/tanga/node_v0.4.2-1_i386.deb"
+  checksum "c407e54d861ec844eda518b4bdd0b065c90726f3acd90f0271889b691577aeb7"
+end
+dpkg_package "node" do
+  version "0.4.2"
+  source node_file
+end
+
+# Coffeescript
+coffee_file = "#{Chef::Config[:file_cache_path]}/coffee.deb"
+remote_file coffee_file do
+  source "https://s3.amazonaws.com/tanga/coffee-script_1.1.0-1_i386.deb"
+  checksum "0b56028e52844ae86874dfb8b8b7a136a39d06e80b176d916d6c0c98e0204fe9"
+end
+dpkg_package "coffee-script" do
+  version "1.1.0"
+  source coffee_file
+end
