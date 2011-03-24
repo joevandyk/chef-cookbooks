81ff85c22fd6e12c5d7a2c55c2569325abe56f60
Joe Van Dyk <joe@fixieconsulting.com>
2011-03-24 15:37:13 -0700

    Added a decent bash prompt

---
+++ bash-vim/files/default/good_prompt.sh
@@ -0,0 +1,57 @@
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+function proml {
+  local        BLUE="\[\033[0;34m\]"
+  local        LIGHT_BLUE="\[\033[1;34m\]"
+  local         RED="\[\033[0;31m\]"
+  local   LIGHT_RED="\[\033[1;31m\]"
+  local       GREEN="\[\033[0;32m\]"
+  local LIGHT_GREEN="\[\033[1;32m\]"
+  local       WHITE="\[\033[1;37m\]"
+  local  LIGHT_GRAY="\[\033[0;37m\]"
+
+  TITLEBAR='\[\033]0;\u@`hostname -f`:\w\007\]'
+
+  PS1="${TITLEBAR}\
+  $LIGHT_BLUE[$LIGHT_RED\u@`hostname -f`:\w$GREEN\$(parse_git_branch)$LIGHT_BLUE]\
+  $LIGHT_GRAY\$ "
+  PS2='> '
+  PS4='+ '
+}
+
+export EDITOR=vim
+export TERM=xterm-color
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines in the history. See bash(1) for more options
+export HISTCONTROL=ignoredups
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
+  debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# some more ls aliases
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if [ -f /etc/bash_completion ]; then
+  . /etc/bash_completion
+fi
+
+proml
--- bash-vim/recipes/default.rb
+++ bash-vim/recipes/default.rb
@@ -1,3 +1,11 @@
 cookbook_file "/etc/profile.d/bash-vim.sh" do
   mode 0644
 end
+
+cookbook_file "/etc/profile.d/good_prompt.sh" do
+  mode 0644
+end
+
+cookbook_file "/etc/profile" do
+  mode 0644
+end
