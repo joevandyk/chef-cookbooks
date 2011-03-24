2d57d8c83fa6e2742e32609de7d4e6e939de3b7c
Joe Van Dyk <joe@fixieconsulting.com>
2011-03-24 11:37:54 -0700

    removed hostname service

--- hostname/recipes/default.rb
+++ hostname/recipes/default.rb
@@ -5,16 +5,10 @@ template "/etc/hosts" do
   mode 0644
 end
 
-service "hostname" do
-  provider Chef::Provider::Service::Upstart
-  service_name "hostname"
-end
-
 template "/etc/hostname" do
   source "hostname.erb"
   owner "root"
   group "root"
   mode "0644"
   action :create
-  notifies :restart, "service[hostname]", :immediately
 end
