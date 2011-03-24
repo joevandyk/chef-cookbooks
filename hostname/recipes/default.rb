cade03dd711d22c1ecf357cc1f09e957d9cb4fb1
Joe Van Dyk <joe@fixieconsulting.com>
2011-03-24 12:21:35 -0700

    updates

--- hostname/recipes/default.rb
+++ hostname/recipes/default.rb
@@ -2,11 +2,17 @@
 node[:short_hostname] = node[:node_hostname].split('.').first
 
 
+execute "restart_hostname" do
+  command "/etc/init.d/hostname restart"
+  action :nothing
+end
+
 template "/etc/hosts" do
   source "hosts.erb"
   owner "root"
   group "root"
   mode 0644
+  notifies :run, :execute => :restart_hostname
 end
 
 template "/etc/hostname" do
@@ -15,4 +21,5 @@ template "/etc/hostname" do
   group "root"
   mode "0644"
   action :create
+  notifies :run, :execute => :restart_hostname
 end
