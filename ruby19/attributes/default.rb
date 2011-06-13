8d1bc18c83f07e9db443ca86522c07b35125c3b7
root <root@test.tanga.com>
2011-06-13 16:42:39 -0700

    updates

--- ruby19/attributes/default.rb
+++ ruby19/attributes/default.rb
@@ -1,5 +1,6 @@
 default[:ruby][:install_path] = "/opt/ruby19"
 default[:ruby][:ruby_bin]     = "/opt/ruby19/bin/ruby"
+default[:ruby][:gem_bin]      = "#{ruby[:install_path]}/bin/gem"
 default[:ruby][:gems_dir]     = "#{ruby[:install_path]}/lib/ruby/gems/1.9.1"
 default[:ruby][:version]      = '1.9.2-p180'
 default[:languages][:ruby][:ruby_bin] = "/opt/ruby19/bin/ruby"
@@ -7,8 +8,8 @@ default[:languages][:ruby][:gems_dir] = "#{ruby[:install_path]}/lib/ruby/gems/1.
 
 case node.automatic[:kernel][:machine]
 when "i686"
-  default[:ruby][:dpkg_url] = "https://s3.amazonaws.com/tanga/tanga-ruby19_1.0.0_i386.deb"
-  default[:ruby][:dpkg_sha] = "fabee1b2316f2eef308a5cb3fca588190610862d10981d5841963dfe995c73e0"
+  default[:ruby][:dpkg_url] = "https://s3.amazonaws.com/tanga/tanga-ruby19_1.0.1_i386.deb"
+  default[:ruby][:dpkg_sha] = "3c20dc1000783cd3bd04925e5a60f3eee3c0d5a2650f4ef1a981f4b46d00d5c7"
 when "x86_64"
   raise "not done yet"
 else
