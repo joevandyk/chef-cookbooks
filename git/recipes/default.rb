f04b01e46da8cbbdfb9844a4f5881c13e1aadd9b
root <root@newapp6.tanga.com>
2011-06-30 21:13:45 -0700

    Sfix

--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -2,6 +2,7 @@ package "git-core"
 
 if node[:git_checkouts]
   node[:git_checkouts].each do |path, options|
+    execute "chown -R #{ options[:user] } /tmp/ssh*" # Let us do ssh forwarding in forked programs
     directory path do
       owner options[:user]
       recursive true
